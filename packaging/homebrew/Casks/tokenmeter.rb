635d8fdb79bc4c96648ea156ccaeb3ca8f0dc219
lteawoo <lteawoo@naver.com>
2026-05-02 19:39:16 +0900

    chore: sync homebrew cask to 0.1.5

--- packaging/homebrew/Casks/tokenmeter.rb
+++ packaging/homebrew/Casks/tokenmeter.rb
@@ -1,6 +1,6 @@
 cask "tokenmeter" do
-  version "0.1.2"
-  sha256 "64f333a1d89081d039f5b7e4318080ac147c0e2361ee2c4449a56913d377d82d"
+  version "0.1.5"
+  sha256 "c063fe7c81bf803a2ec402c51826d0ec26e786ea54e66cb2e61a6833b6f4393d"
 
   url "https://github.com/lteawoo/TokenMeter/releases/download/v#{version}/TokenMeter_#{version}_aarch64.dmg"
   name "TokenMeter"
